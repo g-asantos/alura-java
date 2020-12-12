5410c443cbe0ff39b3986184180735c7a0bda2b7
Guilherme Santos <gui_t900@hotmail.com>
2020-12-11 23:39:48 -0300

    Third chapter half done

---
+++ bytebank-herdado/src/EditorDeVideo.java
@@ -0,0 +1,9 @@
+public class EditorDeVideo extends Funcionario{
+   
+    public double getBonificacao(){
+        System.out.println("Chamando o metodo de bonificacao"
+        + " do editor de video");
+        return 100;
+    }
+
+}
