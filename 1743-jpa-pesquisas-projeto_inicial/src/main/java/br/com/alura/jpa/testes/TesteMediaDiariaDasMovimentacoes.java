8b60f3e4eb02a59d8d1ecc2fdf83132cc648c148
Guilherme Santos <gui_t900@hotmail.com>
2021-01-21 20:34:16 -0300

    Entrando em curso Spring Data

---
+++ 1743-jpa-pesquisas-projeto_inicial/src/main/java/br/com/alura/jpa/testes/TesteMediaDiariaDasMovimentacoes.java
@@ -0,0 +1,27 @@
+package br.com.alura.jpa.testes;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.alura.jpa.dao.MovimentacaoDao;
+import br.com.alura.jpa.modelo.MediaComData;
+
+public class TesteMediaDiariaDasMovimentacoes {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("alura");
+        EntityManager em = emf.createEntityManager();
+        
+        List<MediaComData> mediaDasMovimentacoes = new MovimentacaoDao(em).getMediaDiariaDasMovimentacoes();
+
+        for (MediaComData resultado : mediaDasMovimentacoes) {
+            System.out.println("A media das movimentacoes do dia " + resultado.getDia() + "/" + resultado.getMes() + " é: " + resultado.getValor());
+        }
+        
+        
+
+    }
+}
---
+++ spring-data/src/main/java/br/com/alura/spring/data/orm/Funcionario.java
@@ -0,0 +1,56 @@
+package br.com.alura.spring.data.orm;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="funcionarios")
+public class Funcionario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Double salario;
+
+    private String cpf;
+
+    private LocalDateTime dataContratacao;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDateTime getDataContratacao() {
+        return dataContratacao;
+    }
+
+    public void setDataContratacao(LocalDateTime dataContratacao) {
+        this.dataContratacao = dataContratacao;
+    }
+
+}
