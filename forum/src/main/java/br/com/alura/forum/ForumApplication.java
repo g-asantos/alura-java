2927ae8bcbb1f91c9734a71be0c7a6b11d3f9d2a
Guilherme Santos <gui_t900@hotmail.com>
2021-02-03 19:18:27 -0300

    Second spring boot course half done

--- forum/src/main/java/br/com/alura/forum/ForumApplication.java
+++ forum/src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,8 +2,13 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
+@Cacheable
 public class ForumApplication {
 
 	public static void main(String[] args) {
--- forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -1,13 +1,14 @@
 package br.com.alura.forum.repository;
 
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.alura.forum.modelo.Topico;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
-	List<Topico> findByCurso_Nome(String nomeCurso);
+	Page<Topico> findByCurso_Nome(String nomeCurso, Pageable page);
     
 }
